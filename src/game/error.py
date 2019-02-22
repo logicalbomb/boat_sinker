5b810d34fa17259d6797cab6ae555837414fc85d
Zac Propersi <zacpropersi@Zacs-MacBook-Pro.local>
2019-02-22 10:36:48 -0800

    feat(error): Adding Sentry error tracking

--- src/game/error.py
+++ src/game/error.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+import sentry_sdk
+
+
+sentry_sdk.init("https://61842b75251a4ecbb84748556426aa60@sentry.io/1400606")
+
 
 def handle_create_error(request, error_text):
     # Go back to the index and show an error
