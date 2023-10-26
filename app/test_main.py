8b88f235f204abd9cd0830eee3b4698287c9bc9c
Zac Propersi <3389111+logicalbomb@users.noreply.github.com>
2023-10-26 06:24:42 -0700

    Testing uses its own separate DB \o/

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,26 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from .main import app
+from .database import Base
+from .main import app, get_db
+
+
+SQLALCHEMY_TEST_DB_URL = "sqlite:///./app.test.db"
+
+test_engine = create_engine(SQLALCHEMY_TEST_DB_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+
+Base.metadata.create_all(bind=test_engine)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
