fd57519acf69f8fe5b70b5e86545101db73f87d4
Zac Propersi <3389111+logicalbomb@users.noreply.github.com>
2023-10-26 12:06:48 -0700

    3 data validation (#12)
    * Several bug fixes, min validation on schemas and some tests.
    
    * Testing uses its own separate DB \o/
    
    * Switched to in-mem test DB
    
    * Validating row and col num lengths
    
    * Test rejecting invalid characters
    
    * Validate that start and finish boards match
    
    * Validate the the row and col nums add up to 20

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,101 @@
-from pydantic import BaseModel
+from typing_extensions import Annotated
 
+from pydantic import BaseModel, Field, model_validator
 
+# See test_main.py for examples of these data models
 class GameBase(BaseModel):
-    starting_board: str
-    row_nums: str
-    col_nums: str
+    ##
+    # A game board that is 10x10 and is encoded with the upper left being the character at index 0
+    # and streaming horizontally until you get to the upper right at character index 9. This
+    # continues with the next row starting with the character at index 10. It finally ends with
+    # the lower right corner being the character at index 99.
+    #
+    # The characters in each space represent various items in the game:
+    #   ~ The ocean
+    #   o A ship that is singlespace-wide
+    #   # The center of a multispace-wide ship that works vertically and horizontally, but not both
+    #   < The left end of a horizontal, multispace-wide ship
+    #   > The right end of a horizontal, multispace-wide ship
+    #   ^ The top end of a vertical, multispace-wide ship
+    #   v The bottom end of a vertical, multispace-wide ship
+    #   ? An unknown space, it could be ocean or part of a ship
+    ##
+    starting_board: Annotated[str, Field(
+        min_length=100,
+        max_length=100,
+        pattern='[~o#<>^v?]{100}',
+        repr=True,
+        frozen=True
+    )]
+    ##
+    # The number of ship parts that would appear in the corresponding row. The character at index
+    # 0 represents the top row and the character at index 9 represents the bottom row.
+    ##
+    row_nums: Annotated[str, Field(
+        min_length=10,
+        max_length=10,
+        pattern='[0-8]{10}',
+        repr=True,
+        frozen=True
+    )]
+    ##
+    # The number of ship parts that would appear in the corresponding column. The character at
+    # index 0 represents the left row and the character at index 9 represents the right row.
+    ##
+    col_nums: Annotated[str, Field(
+        min_length=10,
+        max_length=10,
+        pattern='[0-8]{10}',
+        repr=True,
+        frozen=True
+    )]
+
+    @model_validator(mode='after')
+    def check_plausible_num_vals(self) -> 'GameBase':
+        sum = 0
+        for c in self.row_nums:
+            sum += int(c)
+
+        if sum != 20:
+            raise ValueError("Row numbers don't add up to 20, instead {}".format(sum))
+
+        sum = 0
+        for c in self.col_nums:
+            sum += int(c)
+
+        if sum != 20:
+            raise ValueError("Column numbers don't add up to 20, instead {}".format(sum))
+
+        return self
+
 
 class GameCreate(GameBase):
-    finished_board: str
+    ##
+    # A game board that has all of the properties as the starting_board, with one major
+    # exception. The finished board does not have any ? characters because it is solved.
+    ##
+    finished_board: Annotated[str, Field(
+        min_length=100,
+        max_length=100,
+        pattern='[~o#<>^v]{100}',
+        repr=True,
+        frozen=True
+    )]
+
+    @model_validator(mode='after')
+    def check_start_to_finish(self) -> 'GameCreate':
+        # Make sure the starting_board matches the finished_board
+        for i, c in enumerate(self.starting_board):
+            if c == '?' or c == self.finished_board[i]: continue
+            raise ValueError("Start cannot get to finish: {} -/> {} @ {}".format(c, self.finished_board[i], i))
+
+        # TODO: make sure to walk through the steps to solve and produce a matching board
+
+        return self
 
 class Game(GameBase):
+    ##
+    # Database junk :-D
+    ##
     id: int
 
---
+++ app/test_main.py
@@ -0,0 +1,272 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .database import Base
+from .main import app, get_db
+
+
+SQLALCHEMY_TEST_DB_URL = "sqlite://"
+
+def override_get_db():
+    try:
+        test_engine = create_engine(SQLALCHEMY_TEST_DB_URL, connect_args={"check_same_thread": False})
+        Base.metadata.create_all(bind=test_engine)
+        TestSession = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+        yield TestSession()
+    finally:
+        test_engine.dispose()
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+VALID_STARTING_BOARD = (
+    "??????????"
+    "?????????^"
+    "~?????????"
+    "??????????"
+    "??????????"
+    "??????????"
+    "??????????"
+    "??????????"
+    "??????????"
+    "??????????"
+)
+
+VALID_FINISHED_BOARD = (
+    "o~o~~~~~~~"
+    "~~~~~~~~~^"
+    "~~^~~~~~~v"
+    "o~v~~<#>~~"
+    "~~~~~~~~~~"
+    "^~~~~~~~~~"
+    "#~~~~~~~~~"
+    "v~<>~<##>~"
+    "~~~~~~~~~~"
+    "o~~~~~~~~~"
+)
+
+VALID_ROW_NUMS = "2125011701"
+
+VALID_COL_NUMS = "6041022212"
+
+
+
+def test_create_game():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS,
+        },
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 200
+    assert 'id' in response.json().keys()
+
+def test_create_game_with_a_small_starting_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": "?",
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS,
+        },
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_short'
+
+def test_create_game_with_a_large_starting_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD + "?",
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS,
+        },
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_long'
+
+def test_create_game_with_bad_char_in_starting_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD[:99] + "!",
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_pattern_mismatch'
+
+def test_create_game_with_mismatched_boards():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD[:99] + "o",
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'Value error, Start cannot get to finish: o -/> ~ @ 99'
+
+def test_create_game_with_a_small_finished_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": "~",
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS,
+        },
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_short'
+
+def test_create_game_with_a_large_finished_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD + "~",
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS,
+        },
+    )
+    #print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_long'
+
+def test_create_game_with_bad_char_in_finished_board():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": "?" + VALID_FINISHED_BOARD[:99],
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_pattern_mismatch'
+
+def test_create_game_with_small_row_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": "0",
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_short'
+
+def test_create_game_with_large_row_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS + "0",
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_long'
+
+def test_create_game_with_bad_char_in_row_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS[:9] + "9",
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_pattern_mismatch'
+
+def test_create_game_with_too_low_val_in_row_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS[:9] + "0",
+            "col_nums": VALID_COL_NUMS
+        }
+    )
+    print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'Value error, Row numbers don\'t add up to 20, instead 19'
+
+def test_create_game_with_small_col_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": "8"
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_short'
+
+def test_create_game_with_large_col_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": VALID_COL_NUMS + "8"
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_too_long'
+
+def test_create_game_with_bad_char_in_col_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": "*" + VALID_COL_NUMS[:9]
+        }
+    )
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['type'] == 'string_pattern_mismatch'
+
+def test_create_game_with_too_high_val_in_col_nums():
+    response = client.post(
+        "/",
+        json={
+            "starting_board": VALID_STARTING_BOARD,
+            "finished_board": VALID_FINISHED_BOARD,
+            "row_nums": VALID_ROW_NUMS,
+            "col_nums": "8" + VALID_COL_NUMS[:9]
+        }
+    )
+    print("Response is:\n{}".format(response.json()))
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'Value error, Column numbers don\'t add up to 20, instead 26'
+
