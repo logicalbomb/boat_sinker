19dbb6ad40bf9be0b0bccac3a9e70ee0c4cd5c79
Zac Propersi <3389111+logicalbomb@users.noreply.github.com>
2023-10-29 15:24:16 -0700

    Simple template for displaying the game board (#14)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -8,6 +10,8 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
 # Dependency
@@ -22,14 +26,12 @@ def get_db():
 @app.post("/", response_model=schemas.Game)
 def create_game(game: schemas.GameCreate, db: Session = Depends(get_db)):
     # TODO: maybe one day care about the uniqueness of the game
-    # TODO: add minimal validation checks (length, characters, numbers, starting overlaps with finished)
     # TODO: add advanced validation (game is playable, make finished optional and generated)
     return crud.create_game(db=db, game=game)
 
-@app.get("/", response_model=schemas.Game)
-def play_game(db: Session = Depends(get_db)):
-    #TODO: update this to use jinja templates and other "fun" FE stuff
-    return crud.get_random_game(db=db)
+@app.get("/", response_class=HTMLResponse)
+def play_game(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("index.html", {"request": request, "game": crud.get_random_game(db=db)})
 
 @app.get("/game_id}", response_model=schemas.Game)
 def play_specific_game(game_id: int, db: Session = Depends(get_db)):
